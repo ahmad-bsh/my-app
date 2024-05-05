fcf675860e9f9ec4fe2bf80ba4c7914d3420818f
ahmad-bsh <ahmadbshaik1@gmail.com>
2024-05-05 08:24:04 +0530

    add logo and favicon

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,12 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import KodeZnippets from "./_icons/KodeZnippets";
 
 export default function Home() {
   return (
     <main className="flex flex-col">
+      <KodeZnippets width={120} />
       <div className="max-w-xl">
         <Card>
           <CardHeader>
